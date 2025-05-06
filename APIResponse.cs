a1521344eaada5491d14c259d07b1b224f93a880
T Zhou <zhoutao09125@163.com>
2025-05-06 22:05:36 +0800

    添加项目文件。

---
+++ APIResponse.cs
@@ -0,0 +1,27 @@
+﻿namespace NoteService
+{
+    public class APIResponse<T>
+    {
+        public bool Succeed { get; set; }
+        public T Data { get; set; }
+        public string ErrorMessage { get; set; }
+        public static APIResponse<T> Success(T data)
+        {
+            return new APIResponse<T>()
+            {
+                Succeed = true,
+                Data = data,
+                ErrorMessage = string.Empty
+            };
+        }
+        public static APIResponse <T> Failure(string errorMsg)
+        {
+            return new APIResponse<T>()
+            {
+                Succeed = false,
+                Data = default(T),
+                ErrorMessage = errorMsg
+            };
+        }
+    }
+}
---
+++ Controllers/NoteController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using NoteApplication;
+using System.Collections.Generic;
+
+namespace NoteService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NoteController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetNodeInfo()
+        {
+            //加载配置文件
+            try
+            {
+                ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+
+                //添加配置文件路径
+                configurationBuilder.SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json");
+
+                var configuration = configurationBuilder.Build();
+                string baseDir = configuration.GetValue<string>("NoteHomeDir");
+                List<NoteMenu> meuns = new NoteInfoApplication().GetMarkdownNoteFiles(baseDir);
+
+                return new JsonResult(APIResponse<List<NoteMenu>>.Success(meuns));
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(APIResponse<object>.Failure(ex.Message));
+            }
+        }
+    }
+}
